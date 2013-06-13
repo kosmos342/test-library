32bd63f728e9df901dc365eeab6a22f7d77c4c91
kosmos342 <kosmos342@gmail.com>
2013-06-13 05:00:20 +0300

    added missing scripts. moved library's models and views to individual app

--- config.py
+++ config.py
@@ -1,11 +1,15 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+_basedir = os.path.abspath(os.path.dirname(__file__))
 
+DEBUG = False
 
-def rel(*x):
-    return os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+ADMINS = frozenset(['kosmos342@gmail.com'])
+SECRET_KEY = '*\x8c3\xab\xacN:W\x88x\xc0}\xe4\xc5\xc2E\x8f\x17\x00\x82!\x93oa'
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'library.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'app.db')
+DATABASE_CONNECT_OPTIONS = {}
+
+THREADS_PER_PAGE = 4
 
 CSRF_ENABLED = True
-SECRET_KEY = '*\x8c3\xab\xacN:W\x88x\xc0}\xe4\xc5\xc2E\x8f\x17\x00\x82!\x93oa'
+CSRF_SESSION_KEY = 'bcd4621d373cade4e832627b4f6'
